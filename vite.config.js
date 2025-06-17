aadbb9d928bfb6e4be09ebe21be8f4cbd40dd7ea
Kody James <kodyj123@gmail.com>
2025-06-17 03:13:22 -0400

    Fix _*.py displaying correctly

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/Kody-James-Portfolio/',
   plugins: [react()],
+  server: {
+    fs: {
+      allow: ['..'],
+    },
+  },
+  assetsInclude: ['**/_*.py'], // Include files starting with underscore
 });
